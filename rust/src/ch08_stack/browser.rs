fa03f1319a509782e0717c82c984949f057d38b9
cjx <bennycao0916@qq.com>
2022-09-09 17:53:37 +0800

    modifier browser struct field name

--- rust/src/ch08_stack/browser.rs
+++ rust/src/ch08_stack/browser.rs
@@ -1,46 +1,46 @@
 struct Browser {
-    history: Vec<String>,
-    forward: Vec<String>,
+    history_stack: Vec<String>,
+    forward_stack: Vec<String>,
 }
 
 impl Browser {
     pub fn new() -> Self {
-        Self { history: vec![], forward: vec![] }
+        Self { history_stack: vec![], forward_stack: vec![] }
     }
 
     pub fn click_url(&mut self, url: &str) {
-        self.history.push(url.into());
-        self.forward.clear();
+        self.history_stack.push(url.into());
+        self.forward_stack.clear();
     }
 
     pub fn forward(&mut self) {
-        match self.forward.pop() {
+        match self.forward_stack.pop() {
             None => { println!("There is no page for to visited"); }
             Some(url) => {
-                self.history.push(url);
+                self.history_stack.push(url);
                 self.show_page();
             }
         }
     }
 
     pub fn go_back(&mut self) {
-        match self.history.pop() {
+        match self.history_stack.pop() {
             None => { println!("There is no page for to go back"); }
             Some(url) => {
-                self.forward.push(url);
+                self.forward_stack.push(url);
                 self.show_page();
             }
         }
     }
     fn show_page(&self) {
-        println!("Now we in {:?} page", self.history.last().unwrap());
+        println!("Now we in {:?} page", self.history_stack.last().unwrap());
     }
 }
 
 #[test]
 fn test_browser() {
     let mut browser = Browser::new();
-    browser.history = vec![
+    browser.history_stack = vec![
         String::from("index"),
         String::from("home"),
         String::from("user"),
